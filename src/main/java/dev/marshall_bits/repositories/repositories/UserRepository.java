d91c16a86842b8cad03f8e97bffeadeba0b851af
Marshall-Bits <kz_@hotmail.es>
2025-07-15 15:34:24 +0200

    initial code

--- src/main/java/dev/marshall_bits/repositories/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.marshall_bits.repositories.repositories;
-import dev.marshall_bits.repositories.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-    long count();
-}
