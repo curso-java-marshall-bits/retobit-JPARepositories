2a3cec04af03b18889ecd4a518e897e9fe3443f9
Marshall-Bits <kz_@hotmail.es>
2025-07-15 16:17:44 +0200

    task 2 done

--- src/main/java/dev/marshall_bits/repositories/repositories/PostRepository.java
+++ src/main/java/dev/marshall_bits/repositories/repositories/PostRepository.java
@@ -1,7 +1,12 @@
 package dev.marshall_bits.repositories.repositories;
 
 import dev.marshall_bits.repositories.models.Post;
+import dev.marshall_bits.repositories.models.PostCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Post findByTitle(String title);
+    List<Post> findByCategory(PostCategory category);
 }
--- src/main/java/dev/marshall_bits/repositories/repositories/UserRepository.java
+++ src/main/java/dev/marshall_bits/repositories/repositories/UserRepository.java
@@ -4,4 +4,6 @@ import dev.marshall_bits.repositories.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
 }
