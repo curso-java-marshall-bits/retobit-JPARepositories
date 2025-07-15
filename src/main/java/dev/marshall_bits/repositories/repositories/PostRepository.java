3e3f0bbd77a39e3f1148a494e991fe6628817452
Marshall-Bits <kz_@hotmail.es>
2025-07-15 16:39:49 +0200

    task 4 done

--- src/main/java/dev/marshall_bits/repositories/repositories/PostRepository.java
+++ src/main/java/dev/marshall_bits/repositories/repositories/PostRepository.java
@@ -3,10 +3,20 @@ package dev.marshall_bits.repositories.repositories;
 import dev.marshall_bits.repositories.models.Post;
 import dev.marshall_bits.repositories.models.PostCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     Post findByTitle(String title);
     List<Post> findByCategory(PostCategory category);
+    @Query("SELECT p FROM Post p WHERE p.viewCount > 100")
+    List<Post> findPostsWithMoreThan100Views();
+
+    @Query("SELECT p FROM Post p ORDER BY p.createdAt DESC")
+    List<Post> findAllByCreatedAt();
+
+    @Query("SELECT p FROM Post p WHERE p.title LIKE %:keyword%")
+    List<Post> findByTitleContaining(@Param("keyword") String keyword);
 }
